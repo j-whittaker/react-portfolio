4cd3b0bbe5aaf5a965af9e01769cb04fccb355f3
Jacob Whittaker <jakewhitt@gmail.com>
2024-08-26 13:51:34 -0600

    refactored / rethemed home page

--- src/components/ContactWidget.jsx
+++ src/components/ContactWidget.jsx
@@ -1,10 +1,12 @@
+import '../styles/Contact.css'
+
 export default function ContactWidget() {
     return (
             <div className="contact-widget">
                 <h3>Looking to contact Jacob with any business inquiries?</h3>
                 <div className="phone-number">Call or Text at <a href="tel:9193845368">(919)-384-5368</a></div>
                 <div className="contact-email">Email at <a href= "mailto: jakewhitt@gmail.com">jakewhitt@gmail.com </a></div>
-                <div className="linked-or">or Connect with Jacob on <a href="https://www.linkedin.com/in/jacob-whittaker-485473111">LinkedIn</a></div>
+                <div className="linked-or">Connect with Jacob on <a href="https://www.linkedin.com/in/jacob-whittaker-485473111">LinkedIn</a></div>
             </div>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,23 @@
 import ContactWidget from "../components/ContactWidget";
+import { HOME_PAGE } from "../constants/PageConstants";
+import '../styles/Home.css'
 
 export default function HomePage() {
     return (
         <div className="main-content home">
+          <h2>{HOME_PAGE}</h2>
           <div className="content-wrapper two-col">
-            <div className="image-wrapper">
-              <img src="/ProfilePicture.jpg" className="profile-img" alt="Profile Picture"></img>
-            </div>
-            <div className="objective-wrapper">
-                <strong>OBJECTIVE</strong> 
-                <span>I'm looking to apply my problem solving skills to build and maintain excellent software that exceeds expectations. 
-                  I have fullstack experience and a passion for building software that addresses real world problems.
+          <div className="lg-col">
+                <span className="objective">I'm looking to apply my problem solving skills to build and maintain excellent software that exceeds expectations. 
+                  I have full stack experience a passion for building solutions that addresses real world problems.
                 </span>
+                <ContactWidget />
+            </div>
+            <div className="image-wrapper sm-col">
+              <img src="/ProfilePicture.png" className="profile-img" alt="Profile Picture"></img>
             </div>
           </div>
-          <ContactWidget />
+          
         </div>
       );
   }
